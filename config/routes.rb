c7046f2211340c072a0c21c1721d86142973c74c
kashyapSheladiya <kashyap27041993@gmail.com>
2019-04-08 15:34:34 +0200

    Create new template, action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   get 'about', to: 'pages#about'
+
+  resources :articles #resources will give a path for new_article, edit_article, show_article, update_article, index_article, destroy_article.
+                      #To view all the paths or routes in console it can be typed "rake routes"
 end
