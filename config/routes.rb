4112427a957db3ea8143a6781b91984f89acf323
kashyapSheladiya <kashyap27041993@gmail.com>
2019-04-11 13:34:40 +0200

    Sessions, logIn, logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
 
   resources :articles #resources will give a path for new_article, edit_article, show_article, update_article, index_article, destroy_article.
                       #To view all the paths or routes in console it can be typed "rake routes"
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
