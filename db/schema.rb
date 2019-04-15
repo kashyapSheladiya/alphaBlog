3ee62e36e1d0c551440738fea7151490fe50ee35
kashyapSheladiya <kashyap27041993@gmail.com>
2019-04-15 11:45:18 +0200

    Added Admin User functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_133728) do
+ActiveRecord::Schema.define(version: 2019_04_15_091937) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_133728) do
     t.datetime "created_at", null: false
     t.datetime "deleted_at"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
