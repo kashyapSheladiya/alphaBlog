ab00ee95427f0930c302e37ea24d243aa841381d
kashyapSheladiya <kashyap@gmail.com>
2019-04-16 16:18:03 +0200

    Fixed test and update navbar with the categories.

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
     def setup
-        @category = Category.create(name: "sports")
         @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     end
 
